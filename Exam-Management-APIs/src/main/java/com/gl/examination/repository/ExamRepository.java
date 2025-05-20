a709ec7eac9f43e277e10692cdbb0ce87b6fa45d
sakshi-mohinia <164994733+sakshi-mohinia@users.noreply.github.com>
2025-05-20 11:01:11 +0530

    Update ExamRepository.java

--- Exam-Management-APIs/src/main/java/com/gl/examination/repository/ExamRepository.java
+++ Exam-Management-APIs/src/main/java/com/gl/examination/repository/ExamRepository.java
@@ -4,4 +4,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gl.examination.model.Exam;
 
-public interface ExamRepository extends JpaRepository<Exam, Long> {}
+public interface ExamRepository extends JpaRepository<> {}
