2d677fd8b52c3acd917c208eb76f4f57abfd0f3e
sakshi-mohinia <164994733+sakshi-mohinia@users.noreply.github.com>
2025-05-20 10:55:14 +0530

    Delete Gym-Management-APIs directory

--- Gym-Management-APIs/src/main/java/com/gl/examination/repository/ExamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gl.examination.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.examination.model.Exam;
-
-public interface ExamRepository extends JpaRepository<Exam, Long> {}
