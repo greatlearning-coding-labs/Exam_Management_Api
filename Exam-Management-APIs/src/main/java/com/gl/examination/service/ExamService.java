da9b27f2256e77a71d27abea30806cadfdf6bdcc
sakshi-mohinia <164994733+sakshi-mohinia@users.noreply.github.com>
2025-05-20 11:01:43 +0530

    Update ExamService.java

--- Exam-Management-APIs/src/main/java/com/gl/examination/service/ExamService.java
+++ Exam-Management-APIs/src/main/java/com/gl/examination/service/ExamService.java
@@ -8,21 +8,20 @@ import org.springframework.stereotype.Service;
 import com.gl.examination.model.Exam;
 import com.gl.examination.repository.ExamRepository;
 
-@Service
 public class ExamService  {
-    @Autowired
+ 
     private ExamRepository examRepository;
 
     public Exam createExam(Exam exam) {
-        return examRepository.save(exam);
+        
     }
 
 
     public List<Exam> getAllExams() {
-        return examRepository.findAll();
+      
     }
 
     public Exam getExamById(Long id) {
-        return examRepository.findById(id).orElse(null);
+    
     }
 }
